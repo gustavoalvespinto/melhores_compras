10879274b777231de984af481899ad8cf88cdfaf
gustavoalvespinto <gustavoalvespinto65@gmail.com>
2025-03-26 12:26:37 -0300

    Versão 2 notas calculanotamediaprod.py: Nova versão contendo o cálculo da média de avaliação do produto a partir de 2 notas enviadas pelos cliente.

--- calculanotamediaprod.py
+++ calculanotamediaprod.py
@@ -1,3 +1,5 @@
+import statistics
+
 titulo = "Calcula o valor médio de avaliação do produto"
 print(f'{titulo:^50}')
 
@@ -5,11 +7,11 @@ nota1 = int(input("Digite a primeira nota avaliada do produto pelo cliente: "))
 
 nota2 = int(input("Digite a segunda nota avaliada do produto pelo cliente: "))
 
-nota3 = int(input("Digite a terceira nota avaliada do produto pelo cliente: "))
+media = [nota1, nota2]
 
-media = (float(nota1) + float(nota2) + float(nota3))/3
+media = statistics.mean(media)
 
-print("A média alcançada foi: %s" % media)
+print("A média alcançada foi: {}".format(media))
 
 if media > 6:
     print("Parabéns, produto adequado aos clientes!")
